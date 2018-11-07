e0617a386eb62396b2f0be9668802a529e0a91ae
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2018-11-07 16:20:16 -0500

    fixed bug switching between layouts

--- frontend/src/app/dash/dash.component.ts
+++ frontend/src/app/dash/dash.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { AppComponent } from '../app.component';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -11,18 +11,17 @@ import { HttpClient } from '@angular/common/http';
 export class DashComponent implements OnInit {
   layout;
   grids;
-  id: Number;
+  private sub: any;
 
   constructor (private http: HttpClient, 
     private app: AppComponent, 
     private elementRef: ElementRef,
-    private route: ActivatedRoute,
-    private router: Router)  {
+    private route: ActivatedRoute)  {
 
   }
 
-  async getData() {
-    this.layout = await this.http.get(`http://localhost:3000/layouts/${this.id}.json`).toPromise();
+  async getData(id: Number) {
+    this.layout = await this.http.get(`http://localhost:3000/layouts/${id}.json`).toPromise();
     this.grids = this.layout.grids;
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = this.layout.background;
   }
@@ -31,7 +30,10 @@ export class DashComponent implements OnInit {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.app.title = "Dashboard";
-    this.id = this.route.snapshot.params.id;
-    this.getData();
+    this.sub = this.route.params.subscribe(params => {
+      let id = +params['id'];
+      this.getData(id);
+    })
   }
+  
 }
