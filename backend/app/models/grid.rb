7a40734e845674fc72908e45398c1f0b70d67103
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2018-11-07 21:58:00 -0500

    order grids and widgets by their position

--- backend/app/models/grid.rb
+++ backend/app/models/grid.rb
@@ -2,5 +2,5 @@ class Grid < ApplicationRecord
     has_many :layout_grids, dependent: :delete_all
     has_many :layouts, through: :layout_grids
     has_many :grid_widgets, dependent: :delete_all
-    has_many :widgets, through: :grid_widgets
+    has_many :widgets, -> {order 'grid_widgets.position' }, through: :grid_widgets
 end
--- backend/app/models/layout.rb
+++ backend/app/models/layout.rb
@@ -1,4 +1,4 @@
 class Layout < ApplicationRecord
     has_many :layout_grids, dependent: :delete_all
-    has_many :grids, through: :layout_grids
+    has_many :grids, -> { order 'layout_grids.position' }, through: :layout_grids
 end
