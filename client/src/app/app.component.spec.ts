a70abb66accb230a4478b3007f179da8ee41a3c5
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2020-02-22 20:34:11 -0500

    implemented appcomponent test

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,11 +1,64 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatGridListModule, MatMenuModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule, MatFormFieldModule, MatInputModule, MatSnackBarModule, MatSelectModule, MatCheckboxModule, MatTabsModule } from '@angular/material';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { DashComponent } from './dash/dash.component';
+import { EditorWidgetComponent } from './editor-widget/editor-widget.component';
+import { NewWidgetComponent } from './new-widget/new-widget.component';
+import { EditWidgetComponent } from './edit-widget/edit-widget.component';
+import { EditorGridComponent } from './editor-grid/editor-grid.component';
+import { NewGridComponent } from './new-grid/new-grid.component';
+import { EditGridComponent } from './edit-grid/edit-grid.component';
+import { EditLayoutComponent } from './edit-layout/edit-layout.component';
+import { EditorLayoutComponent } from './editor-layout/editor-layout.component';
+import { NewLayoutComponent } from './new-layout/new-layout.component';
+import { GridComponent } from './grid/grid.component';
+import { WidgetComponent } from './widget/widget.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        DashComponent,
+        GridComponent,
+        WidgetComponent,
+        EditorWidgetComponent,
+        EditorGridComponent,
+        EditorLayoutComponent,
+        EditGridComponent,
+        EditWidgetComponent,
+        EditLayoutComponent,
+        NewLayoutComponent,
+        NewGridComponent,
+        NewWidgetComponent
       ],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatGridListModule,
+        NgxChartsModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSnackBarModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatTabsModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +66,9 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Login'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to frontend!');
+    expect(app.title).toEqual('Login');
   }));
 });
