092713b2477041c632891a79a323cb761835ba46
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2020-02-22 21:28:49 -0500

    implemented gridcomponent testing

--- client/src/app/grid/grid.component.spec.ts
+++ client/src/app/grid/grid.component.spec.ts
@@ -1,21 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GridComponent } from './grid.component';
+import { WidgetComponent } from '../widget/widget.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatGridListModule } from '@angular/material';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { element } from 'protractor';
 
 describe('GridComponent', () => {
   let component: GridComponent;
   let fixture: ComponentFixture<GridComponent>;
+  const grid = {
+    id: 1
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GridComponent ]
+      declarations: [ 
+        GridComponent,
+        WidgetComponent 
+      ],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatGridListModule,
+        NgxChartsModule
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GridComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
+    component.grid = grid;
     fixture.detectChanges();
   });
 
