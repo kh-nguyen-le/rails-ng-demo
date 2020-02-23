5999c4c730177a340e5e4dd9bfb190a65e34f8b3
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2020-02-22 20:55:30 -0500

    implemented dashcomponent testing

--- client/src/app/dash/dash.component.spec.ts
+++ client/src/app/dash/dash.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashComponent } from './dash.component';
+import { GridComponent } from '../grid/grid.component';
+import { WidgetComponent } from '../widget/widget.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatGridListModule, MatTabsModule } from '@angular/material';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { AppComponent } from '../app.component';
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('DashComponent', () => {
   let component: DashComponent;
@@ -8,7 +17,23 @@ describe('DashComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashComponent ]
+      declarations: [ 
+        DashComponent,
+        GridComponent,
+        WidgetComponent 
+      ],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatGridListModule,
+        NgxChartsModule,
+        MatTabsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        AppComponent,
+      ]
     })
     .compileComponents();
   }));
