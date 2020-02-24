6ef957dc126e7e581098dc354dc2a7ba99fae073
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2020-02-24 03:38:29 -0500

    Applied missing material migration

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,9 +11,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { EditorLayoutComponent } from './editor-layout/editor-layout.component';
 import { EditorGridComponent } from './editor-grid/editor-grid.component';
 import { EditorWidgetComponent } from './editor-widget/editor-widget.component';
-import { MatGridListModule, MatMenuModule, MatToolbarModule, MatButtonModule,
-  MatIconModule, MatListModule, MatFormFieldModule, MatInputModule,
-  MatSnackBarModule, MatSelectModule, MatCheckboxModule, MatTabsModule } from '@angular/material/';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { NewLayoutComponent } from './new-layout/new-layout.component';
 import { NewGridComponent } from './new-grid/new-grid.component';
