8a1481dbc6ed3a7c1999ffd5fb074157b4de08a4
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2020-02-29 20:08:25 -0500

    correct channel id

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,7 +1,7 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
     def component_channel
-      "#{channel_name}_#{params['type']}"
+      "#{channel_name}_#{params['type']}_#{params['id']}"
     end
   end
 end
