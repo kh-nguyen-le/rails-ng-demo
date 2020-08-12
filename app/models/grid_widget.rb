803bc5861c17310e9dab19293d6e8208e705e489
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2020-08-12 18:50:15 -0400

    grid_widget specs + validations

--- app/models/grid_widget.rb
+++ app/models/grid_widget.rb
@@ -1,4 +1,5 @@
 class GridWidget < ApplicationRecord
   belongs_to :grid
   belongs_to :widget
+  validates :position, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/grid_widget_spec.rb
+++ spec/models/grid_widget_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
+require 'common_setup'
 
 RSpec.describe GridWidget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new({
+                          position: 0,
+                          grid_id: -1,
+                          widget_id: -1
+                        })
+  end
+
+  it { is_expected.to be_invalid }
+
+  context 'with existing grid and widget' do
+    include_context 'widget'
+    include_context 'grid'
+
+    before(:example) do
+      subject.grid_id = @grid.id
+      subject.widget_id = @widget.id
+    end
+
+    it { is_expected.to be_valid }
+
+    it 'is invalid with negative position' do
+      subject.position = -1
+      expect(subject).to be_invalid
+    end
+  end
 end
