47daf459a421f12830f34fa2f36dac50a9e1b515
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2020-08-11 22:26:01 -0400

    grid specs + validations to model

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -3,4 +3,6 @@ class Grid < ApplicationRecord
   has_many :layouts, through: :layout_grids
   has_many :grid_widgets, dependent: :delete_all
   has_many :widgets, -> { order 'grid_widgets.position' }, through: :grid_widgets
+  validates_presence_of :name
+  validates :col, numericality: { greater_than: 0 }
 end
--- spec/models/grid_spec.rb
+++ spec/models/grid_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Grid, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new({
+                          name: 'Grid',
+                          title: 'Singleton',
+                          col: 1
+                        })
+  end
+
+  it { is_expected.to have_attributes(name: 'Grid', col: 1) }
+
+  it 'is invalid without a name' do
+    subject.name = ''
+    expect(subject).to be_invalid
+  end
+
+  it 'has at least 1 column' do
+    subject.col = 0
+    expect(subject).to_not be_valid
+  end
 end
