2267b8939eb5d124c647001c62a9788914dce244
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2020-08-12 21:10:34 -0400

    synchro specs + channel modifications

--- app/channels/synchro_channel.rb
+++ app/channels/synchro_channel.rb
@@ -1,10 +1,15 @@
 class SynchroChannel < ApplicationCable::Channel
   def subscribed
+    type = params[:type]
+    id = params[:id]
+    reject unless type == 'widget' || type == 'grid' || type == 'layout'
+    reject unless id > 0
     stream_from component_channel
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 
   def receive(data)
--- spec/channels/synchro_channel_spec.rb
+++ spec/channels/synchro_channel_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe SynchroChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'successfully subscribes' do
+    subscribe type: 'widget', id: 1
+    expect(subscription).to be_confirmed
+    expect(subscription).to have_stream_from('synchro_widget_1')
+  end
+
+  it 'rejects invalid types' do
+    subscribe type: 'test', id: 1
+    expect(subscription).to be_rejected
+  end
+
+  it 'rejects invalid ids' do
+    subscribe type: 'grid', id: -1
+    expect(subscription).to be_rejected
+  end
+
+  it 'relays component data' do
+    subscribe type: 'layout', id: 1
+    expect { perform :receive, name: 'Layout' }.to broadcast_to('synchro_layout_1')
+  end
 end
