0f9ddb0ecc6a00e1ccab8e5b4847689042f4482a
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2020-08-21 15:56:11 -0400

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,52 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_227_183_535) do
+ActiveRecord::Schema.define(version: 2020_02_27_183535) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'grid_widgets', force: :cascade do |t|
-    t.integer 'position'
-    t.integer 'length'
-    t.integer 'width'
-    t.integer 'grid_id'
-    t.integer 'widget_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[grid_id widget_id], name: 'index_grid_widgets_on_grid_id_and_widget_id'
+  create_table "grid_widgets", force: :cascade do |t|
+    t.integer "position"
+    t.integer "length"
+    t.integer "width"
+    t.integer "grid_id"
+    t.integer "widget_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["grid_id", "widget_id"], name: "index_grid_widgets_on_grid_id_and_widget_id"
   end
 
-  create_table 'grids', force: :cascade do |t|
-    t.string 'name'
-    t.string 'title'
-    t.integer 'col'
-    t.string 'size'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "grids", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.integer "col"
+    t.string "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'layout_grids', force: :cascade do |t|
-    t.integer 'position'
-    t.integer 'layout_id'
-    t.integer 'grid_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[layout_id grid_id], name: 'index_layout_grids_on_layout_id_and_grid_id'
+  create_table "layout_grids", force: :cascade do |t|
+    t.integer "position"
+    t.integer "layout_id"
+    t.integer "grid_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["layout_id", "grid_id"], name: "index_layout_grids_on_layout_id_and_grid_id"
   end
 
-  create_table 'layouts', force: :cascade do |t|
-    t.string 'name'
-    t.string 'background'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'duration'
+  create_table "layouts", force: :cascade do |t|
+    t.string "name"
+    t.string "background"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "duration"
   end
 
-  create_table 'widgets', force: :cascade do |t|
-    t.string 'name'
-    t.jsonb 'results'
-    t.jsonb 'config'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "widgets", force: :cascade do |t|
+    t.string "name"
+    t.jsonb "results"
+    t.jsonb "config"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
