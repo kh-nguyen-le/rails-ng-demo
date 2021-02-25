64a66d245d66f8d1b8cea5dc739c5afcf5b2a8a6
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2021-02-24 19:36:23 -0500

    Add factorybot to rspec suites

--- spec/common_setup.rb
+++ spec/common_setup.rb
@@ -1,30 +1,18 @@
 RSpec.shared_context 'widget' do
   before(:context) do
-    config = {
-      widgetType: 'line',
-      showXAxis: 'true',
-      showYAxis: 'true',
-      gradient: 'false',
-      showLegend: 'true',
-      showXAxisLabel: 'true',
-      xAxisLabel: 'Country',
-      showYAxisLabel: 'true',
-      yAxisLabel: 'Population',
-      autoScale: 'true'
-    }
-    @widget = Widget.create!(name: 'Widget', config: config)
+    @widget = create(:widget)
   end
 end
 
 RSpec.shared_context 'grid' do
   before(:context) do
-    @grid = Grid.create!(name: 'Grid', col: 1)
+    @grid = create(:grid)
   end
 end
 
 RSpec.shared_context 'layout' do
   before(:context) do
-    @layout = Layout.create!(name: 'Layout')
+    @layout = create(:layout)
   end
 end
 
