65b7bda670b54b7984208ffd345611a7263b3c4e
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2021-03-03 15:37:04 -0500

    update rails to 6.1.3

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :layouts
   resources :layout_grids
   resources :grid_widgets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
