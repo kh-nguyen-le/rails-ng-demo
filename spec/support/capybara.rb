9ec656e814c35235b6a2178f0e27cc4d9945abbd
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2021-08-06 11:59:05 -0400

    set rails env to test

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -23,8 +23,7 @@ end
 Capybara.javascript_driver = :selenium_remote_chrome
 Capybara.app_host = 'http://frontend:4200'
 Capybara.asset_host = 'http://api:3000'
-
-Capybara.server_port = 3000
+Capybara.always_include_port = true
 
 Capybara::Screenshot.register_driver(:selenium_remote_chrome) do |driver, path|
   driver.browser.save_screenshot(path)
