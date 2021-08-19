d892a445794cbf0acb9febbd51d17c821aa74905
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2021-08-19 16:54:34 -0400

    converted layout editor fetch to ngrx

--- client/src/app/editor/editor-layout/editor-layout.component.ts
+++ client/src/app/editor/editor-layout/editor-layout.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../../shared/config.service';
 import { Title } from '@angular/platform-browser';
 import { Layout } from '../../shared/models/layout.model';
+import { AppState } from 'src/app/shared/state';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { LayoutActions, LayoutSelectors } from 'src/app/shared/state/display-state';
 
 @Component({
   selector: 'app-editor-layout',
@@ -9,9 +13,9 @@ import { Layout } from '../../shared/models/layout.model';
   styleUrls: ['./editor-layout.component.css'],
 })
 export class EditorLayoutComponent implements OnInit {
-  layouts: Layout[];
+  layouts$: Observable<Layout[]>;
 
-  constructor(private titleService: Title, private conf: ConfigService) {
+  constructor(private titleService: Title, private conf: ConfigService, private store: Store<AppState>) {
     this.titleService.setTitle('Editor - Layouts');
   }
 
@@ -20,10 +24,11 @@ export class EditorLayoutComponent implements OnInit {
   }
 
   getLayouts(): void {
-    this.conf.getLayouts().subscribe((res) => (this.layouts = res));
+    this.store.dispatch(LayoutActions.loadLayouts());
   }
 
   ngOnInit(): void {
+    this.layouts$ = this.store.select(LayoutSelectors.selectAllLayouts);
     this.getLayouts();
   }
 }
