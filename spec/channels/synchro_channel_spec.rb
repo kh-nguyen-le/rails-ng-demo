61466b4506676235bd276681dadf5b556c23b85f
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2021-09-10 13:02:59 -0400

    update synchro specs

--- spec/channels/synchro_channel_spec.rb
+++ spec/channels/synchro_channel_spec.rb
@@ -2,23 +2,13 @@ require 'rails_helper'
 
 RSpec.describe SynchroChannel, type: :channel do
   it 'successfully subscribes' do
-    subscribe type: 'widget', id: 1
+    subscribe 
     expect(subscription).to be_confirmed
-    expect(subscription).to have_stream_from('synchro_widget_1')
-  end
-
-  it 'rejects invalid types' do
-    subscribe type: 'test', id: 1
-    expect(subscription).to be_rejected
-  end
-
-  it 'rejects invalid ids' do
-    subscribe type: 'grid', id: -1
-    expect(subscription).to be_rejected
+    expect(subscription).to have_stream_from('synchro')
   end
 
   it 'relays component data' do
-    subscribe type: 'layout', id: 1
-    expect { perform :receive, name: 'Layout' }.to broadcast_to('synchro_layout_1')
+    subscribe
+    expect { perform :receive, name: 'Layout' }.to broadcast_to('synchro')
   end
 end
