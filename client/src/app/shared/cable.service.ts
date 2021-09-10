264cfcf7a0c63c4b77104c9a90e8062650415602
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2021-09-10 12:59:51 -0400

    implement layout and widget live update

--- client/src/app/shared/cable.service.ts
+++ client/src/app/shared/cable.service.ts
@@ -5,8 +5,12 @@ import { DisplayObject } from './models/displayobject.model';
 import { Store } from '@ngrx/store';
 import { AppState } from './state';
 import { fetchGrid } from './state/display-state/grid.actions';
-import { GridSelectors } from './state/display-state';
+import { GridSelectors, LayoutSelectors } from './state/display-state';
 import { first } from 'rxjs/operators';
+import { Grid } from './models/grid.model';
+import { fetchWidget } from './state/display-state/widget.actions';
+import { fetchLayout } from './state/display-state/layout.actions';
+import { Layout } from './models/layout.model';
 
 @Injectable({
   providedIn: 'root',
@@ -28,30 +32,38 @@ export class CableService {
         return ActionCable.logger.log('Synchro rejected');
       },
       received: (data: DisplayObject) => {
-        const id$ = this.store.select(GridSelectors.getSelectedGridId);
-        let grid_id;
-        id$.pipe(first()).subscribe(id => { grid_id = id });
-        ActionCable.logger.log(grid_id);
+        const grid$ = this.store.select(GridSelectors.selectCurrentGrid);
+        let current_grid: Grid;
+        grid$.pipe(first()).subscribe((data) => {
+          current_grid = data;
+        });
+        const layout$ = this.store.select(LayoutSelectors.selectCurrentLayout);
+        let current_layout: Layout;
+        layout$.pipe(first()).subscribe((data) => {
+          current_layout = data;
+        });
         switch (data.kind) {
           case 'layout':
-            for (const grid of data.grids) {
-              if (grid.id == grid_id) {
-                ActionCable.logger.log('received layout');
-                this.store.dispatch(fetchGrid({ id: grid_id }));
+            if (data.id == current_layout.id) {
+              ActionCable.logger.log('received layout');
+              for (const grid of data.grids) {
+                this.store.dispatch(fetchGrid({ id: grid.id }));
               }
+              this.store.dispatch(fetchLayout({ id: data.id }));
             }
             break;
           case 'grid':
-            if (data.id == grid_id) {
+            if (data.id == current_grid.id) {
               ActionCable.logger.log('received grid');
-              this.store.dispatch(fetchGrid({ id: grid_id }));
+              this.store.dispatch(fetchGrid({ id: current_grid.id }));
             }
             break;
           case 'widget':
-            for (const grid of data.grids) {
-              if (grid.id == grid_id) {
+            for (const widget of current_grid.widgets) {
+              if (widget.id == data.id) {
                 ActionCable.logger.log('received widget');
-                this.store.dispatch(fetchGrid({ id: grid_id }));
+                this.store.dispatch(fetchGrid({ id: current_grid.id }));
+                this.store.dispatch(fetchWidget({ id: data.id }));
               }
             }
             break;
