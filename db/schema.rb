992ae525f26b338f83d5c7220eb32db62ffc0e78
John Le <kh.nguyen.le@gmail.com>
2023-03-07 15:34:35 -0500

    update ruby and rails version

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_183535) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_02_27_183535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,8 +20,8 @@ ActiveRecord::Schema.define(version: 2020_02_27_183535) do
     t.integer "width"
     t.integer "grid_id"
     t.integer "widget_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["grid_id", "widget_id"], name: "index_grid_widgets_on_grid_id_and_widget_id"
   end
 
@@ -31,24 +30,24 @@ ActiveRecord::Schema.define(version: 2020_02_27_183535) do
     t.string "title"
     t.integer "col"
     t.string "size"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "layout_grids", force: :cascade do |t|
     t.integer "position"
     t.integer "layout_id"
     t.integer "grid_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["layout_id", "grid_id"], name: "index_layout_grids_on_layout_id_and_grid_id"
   end
 
   create_table "layouts", force: :cascade do |t|
     t.string "name"
     t.string "background"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "duration"
   end
 
@@ -56,8 +55,8 @@ ActiveRecord::Schema.define(version: 2020_02_27_183535) do
     t.string "name"
     t.jsonb "results"
     t.jsonb "config"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
 end
