808e1ab9b6448609e58454d4051a1baa64d4cdb4
John Le <kh.nguyen.le@gmail.com>
2023-03-07 14:43:04 -0500

    fix jest tests

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-preset-angular',
+  globalSetup: 'jest-preset-angular/global-setup',
   roots: ['<rootDir>'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
   setupFilesAfterEnv: ['<rootDir>/setupJest.ts'],
